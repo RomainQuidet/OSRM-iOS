20745de11b304a1c3f0d24be35651b68e51b3ad0
Romain Quidet <contact@xdappfactory.com>
2017-04-01 14:59:11 +0200

    Initial Commit

---
+++ OSRM-iOS/OSRM-iOS.h
@@ -0,0 +1,19 @@
+//
+//  OSRM-iOS.h
+//  OSRM-iOS
+//
+//  Created by Romain Quidet on 01/04/2017.
+//  Copyright © 2017 XDAppfactory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OSRM-iOS.
+FOUNDATION_EXPORT double OSRM_iOSVersionNumber;
+
+//! Project version string for OSRM-iOS.
+FOUNDATION_EXPORT const unsigned char OSRM_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OSRM_iOS/PublicHeader.h>
+
+
